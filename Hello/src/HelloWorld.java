cb851831177eff52d0dfb5db6cf49a69fd296aee
pcwang <pcwang@cht.com.tw>
2020-03-12 22:24:16 +0800

    modi again(Hello2)

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("right...");
+		System.out.println("right right...");
 
 	}
 
